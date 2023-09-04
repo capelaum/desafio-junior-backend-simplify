720f71c9f7d3a8820b45ac724768d3e6e018a277
capelaum <thecapellett@gmail.com>
2023-09-04 16:56:33 -0300

    feat: tasks info section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { ButtonToggleTheme } from '@/components/ButtonToggleTheme'
 import { FormCreateTask } from '@/components/FormCreateTask'
+import { Badge } from '@/components/ui/badge'
+import { Separator } from '@/components/ui/separator'
 import { CheckCheck } from 'lucide-react'
 
 export default function Home() {
   return (
     <main className="min-h-screen">
-      <div className="relative flex items-center justify-center border-b border-b-slate-700 py-20">
+      <div className="relative flex items-center justify-center py-20">
         <h1 className="flex scroll-m-20 items-center gap-3 text-3xl font-extrabold tracking-tight lg:text-4xl">
           <CheckCheck className="h-8 w-8" />
           Symplify Tarefas
@@ -15,8 +17,26 @@ export default function Home() {
         </div>
       </div>
 
+      <Separator />
+
       <div className="container py-6">
         <FormCreateTask />
+
+        <section className="mt-8 flex flex-col">
+          <div className="flex justify-between">
+            <span className="flex items-center gap-3 font-medium text-primary">
+              Tarefas criadas
+              <Badge variant="secondary">0</Badge>
+            </span>
+
+            <span className="flex items-center gap-3 font-medium text-primary">
+              Concluídas
+              <Badge variant="secondary">0</Badge>
+            </span>
+          </div>
+
+          <Separator className="mt-4" />
+        </section>
       </div>
     </main>
   )
