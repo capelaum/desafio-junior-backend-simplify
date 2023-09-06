64fd65fc86af105e338fb1dae22c03893b0f617c
capelaum <thecapellett@gmail.com>
2023-09-06 03:34:06 -0300

    fix: theme initial render

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-BR" className={roboto.variable}>
+    <html lang="pt-BR" className={roboto.variable} suppressHydrationWarning>
       <body>
         <Providers>{children}</Providers>
       </body>
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -4,7 +4,7 @@ import { Toaster } from '@/components/ui/toaster'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
-import { ReactNode, useEffect, useState } from 'react'
+import { ReactNode } from 'react'
 
 interface ProvidersProps {
   children: ReactNode
@@ -13,22 +13,12 @@ interface ProvidersProps {
 const queryClient = new QueryClient()
 
 export default function Providers({ children }: ProvidersProps) {
-  const [mounted, setMounted] = useState(false)
-
-  useEffect(() => {
-    setMounted(true)
-  }, [])
-
   return (
     <SessionProvider>
       <QueryClientProvider client={queryClient}>
-        {!mounted ? (
-          children
-        ) : (
-          <ThemeProvider attribute="class" storageKey="@simplify-todo:theme">
-            {children}
-          </ThemeProvider>
-        )}
+        <ThemeProvider attribute="class" storageKey="@simplify-todo:theme">
+          {children}
+        </ThemeProvider>
         <Toaster />
       </QueryClientProvider>
     </SessionProvider>
