48388bc69c2daec0f0fc8f4d872931e34c6bdfcd
capelaum <thecapellett@gmail.com>
2023-09-06 04:26:44 -0300

    feat: task list skeleton

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -5,12 +5,13 @@ import { ClipboardList } from 'lucide-react'
 import { TaskItem } from './TaskItem'
 import { Badge } from './ui/badge'
 import { Separator } from './ui/separator'
+import { Skeleton } from './ui/skeleton'
 
 export function TaskList() {
   const { isLoading, data } = useTasks()
 
   if (isLoading) {
-    return null
+    return <TaskListSkeleton />
   }
 
   return (
@@ -45,6 +46,32 @@ export function TaskList() {
 
       <section className="mt-4 flex flex-col items-center gap-3">
         {data?.tasks.map((task) => <TaskItem key={task.id} task={task} />)}
+      </section>
+    </section>
+  )
+}
+
+export function TaskListSkeleton() {
+  return (
+    <section className="flex flex-col">
+      <div className="flex flex-col justify-between gap-5 sm:flex-row">
+        <span className="flex items-center gap-3 font-medium text-violet-500 dark:text-violet-400">
+          Tarefas criadas
+          <Skeleton className="h-5 w-7 animate-pulse rounded-full" />
+        </span>
+
+        <span className="flex items-center gap-3 font-medium text-violet-500 dark:text-violet-400">
+          Concluídas
+          <Skeleton className="h-5 w-7 animate-pulse rounded-full" />
+        </span>
+      </div>
+
+      <Separator className="mt-4" />
+
+      <section className="mt-4 flex flex-col items-center gap-3">
+        <Skeleton className="h-[58px] w-full animate-pulse rounded-lg shadow-md" />
+        <Skeleton className="h-[58px] w-full animate-pulse rounded-lg shadow-md" />
+        <Skeleton className="h-[58px] w-full animate-pulse rounded-lg shadow-md" />
       </section>
     </section>
   )
